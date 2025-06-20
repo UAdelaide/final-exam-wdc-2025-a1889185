df8ce865a17b1f37b6b49b8f664fd868095469d2
James Walters <unknown@student.adelaide.edu.au>
2025-06-20 01:26:48 +0000

    [automatic commit] 2025-06-20 01:26:48 UTC 1.158.159.156

--- part1/dataToInsert.sql
+++ part1/dataToInsert.sql
@@ -2,10 +2,10 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('shaggy', 'shaggy@mysteryinc.com', 'zoinkssc00b', 'owner'),
-('bruce', 'brucewayne@wayneindustries.com', 'batman123', 'owner')
+('shaggy123', 'shaggy@mysteryinc.com', 'zoinkssc00b', 'owner'),
+('bruce123', 'brucewayne@wayneindustries.com', 'batman123', 'owner')
 
 INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
-((SELECT user_id FROM Users WHERE username = ''
+((SELECT user_id FROM Users WHERE username = 'shaggy123'), ''
