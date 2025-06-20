1f9246edd8da14d33818ebc5218fd80a8183c69d
James Walters <unknown@student.adelaide.edu.au>
2025-06-20 01:28:31 +0000

    [automatic commit] 2025-06-20 01:28:30 UTC 1.158.159.156

--- part1/dataToInsert.sql
+++ part1/dataToInsert.sql
@@ -9,3 +9,4 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
 ((SELECT user_id FROM Users WHERE username = 'shaggy123'), 'Scooby', 'large').
+((SELECT user_id FROM Users WHERE username = ))
