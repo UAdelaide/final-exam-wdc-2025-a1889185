c8f37f86b6c5649db1758985b3d178016c37f3f6
James Walters <unknown@student.adelaide.edu.au>
2025-06-20 05:49:49 +0000

    [automatic commit] 2025-06-20 05:49:49 UTC 1.158.159.156

--- part1/app.js
+++ part1/app.js
@@ -214,7 +214,7 @@ app.get('/api/walkers/summary', async (req, res) => {
       LEFT JOIN WalkRequests ON WalkApplications.request_id = WalkRequests.request_id AND WalkRequests.status = 'completed'
       LEFT JOIN WalkRatings ON WalkRequests.request_id = WalkRatings.request_id AND WalkRatings.walker_id = Users.user_id
       WHERE Users.role = 'walker'
-      
+      GROUP BY Users.user_id, Users.username
       `);
     res.json(walkers);
   } catch (err) {
