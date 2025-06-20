a8650d8a9ac5f8c846e342097f73f534f293e0ee
James Walters <unknown@student.adelaide.edu.au>
2025-06-20 01:41:10 +0000

    [automatic commit] 2025-06-20 01:41:09 UTC 1.158.159.156

--- part1/dataToInsert.sql
+++ part1/dataToInsert.sql
@@ -3,14 +3,18 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('bobwalker', 'bob@example', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
 ('shaggy123', 'shaggy@mysteryinc.com', 'zoinkssc00b', 'owner'),
-('bruce123', 'brucewayne@wayneindustries.com', 'batman123', 'owner')
+('bruce123', 'brucewayne@wayneindustries.com', 'batman123', 'owner');
 
 INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
 ((SELECT user_id FROM Users WHERE username = 'shaggy123'), 'Scooby', 'large'),
 ((SELECT user_id FROM Users WHERE username = 'bruce123'), 'Ace', 'medium'),
-((SELECT user_id FROM Users WHERE username = 'alice123'), 'Min', 'medium')
+((SELECT user_id FROM Users WHERE username = 'alice123'), 'Min', 'medium');
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
-((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-10 08:00:00', 30, 'Parklands', 'open')
+((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-10 08:00:00', 30, 'Parklands', 'open');
