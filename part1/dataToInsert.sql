b2be029394daea160c6f76652b283fa3d19c32d6
James Walters <unknown@student.adelaide.edu.au>
2025-06-20 01:21:40 +0000

    [automatic commit] 2025-06-20 01:21:40 UTC 1.158.159.156

--- part1/dataToInsert.sql
+++ part1/dataToInsert.sql
@@ -2,5 +2,5 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('Shaggy', 'mystery', 'zoinkssc00b', 'owner'),
+('Shaggy', 'shaggy@mysteryinc', 'zoinkssc00b', 'owner'),
 ('')
