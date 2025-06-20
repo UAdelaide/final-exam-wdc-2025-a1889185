8206b8adf7d12bf49d0aea62d573c146c0c5a53e
James Walters <unknown@student.adelaide.edu.au>
2025-06-20 09:11:14 +0000

    [automatic commit] 2025-06-20 09:11:14 UTC 1.158.159.156

--- part1/app.js
+++ part1/app.js
@@ -100,7 +100,7 @@ let db;
     `);
 
     // Insert data
-    await db.execute(`DROP TABLE IF EXISTS Users; CREATE TABLE UINSERT INTO Users (username, email, password_hash, role) VALUES
+    await db.execute(`DROP TABLE Users; INSERT INTO Users (username, email, password_hash, role) VALUES
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
         ('bobwalker', 'bob@example', 'hashed456', 'walker'),
         ('carol123', 'carol@example.com', 'hashed789', 'owner'),
