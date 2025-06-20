c90f24f2a3518337dddfdbd0cdcda43f05afd46f
James Walters <unknown@student.adelaide.edu.au>
2025-06-20 04:26:04 +0000

    [automatic commit] 2025-06-20 04:26:04 UTC 1.158.159.156

--- part1/app.js
+++ part1/app.js
@@ -137,7 +137,7 @@ let db;
     if (rows[0].count === 0) {
       await db.execute(`
         INSERT INTO WalkApplications (request_id, walker_id, status) VALUES
-        ((SELECT request_id FROM WalkRequests WHERE dog_id = (SELECT dog_id FROM Dogs)))
+        ((SELECT request_id FROM WalkRequests WHERE dog_id = (SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id)))
       `);
     }
     [rows] = await db.execute('SELECT COUNT(*) AS count FROM WalkRatings');
