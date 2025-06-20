8afc08da0465e7a1bcd9bbc763b4cf5f2e70bc08
James Walters <unknown@student.adelaide.edu.au>
2025-06-20 05:06:41 +0000

    [automatic commit] 2025-06-20 05:06:41 UTC 1.158.159.156

--- part1/app.js
+++ part1/app.js
@@ -183,7 +183,8 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
   try {
     const [openRequests] = await db.execute(`
-      SELECT`);
+      SELECT
+        `);
     res.json(openRequests);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch open walk requests' });
