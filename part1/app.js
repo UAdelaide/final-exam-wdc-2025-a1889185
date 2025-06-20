6d946e0d68887cf953668b18df21417423a89c51
James Walters <unknown@student.adelaide.edu.au>
2025-06-20 04:48:05 +0000

    [automatic commit] 2025-06-20 04:48:05 UTC 1.158.159.156

--- part1/app.js
+++ part1/app.js
@@ -166,7 +166,7 @@ let db;
 
 app.get('/api/dogs', async (req, res) => {
   try {
-    const [dogs] = await db.execute('SELECT name, size FROM Dogs');
+    const [dogs] = await db.execute('SELECT name, size FROM Dogs LEFT JOIN ');
     res.json(dogs);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch dogs' });
