74b0c02938231c02e1ee87750c580ab6651eef8e
James Walters <unknown@student.adelaide.edu.au>
2025-06-20 03:46:53 +0000

    [automatic commit] 2025-06-20 03:46:53 UTC 1.158.159.156

--- part1/app.js
+++ part1/app.js
@@ -100,7 +100,7 @@ let db;
     `);
 
     // Insert data if table is empty
-    let [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
+    let [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (rows[0].count === 0) {
       await db.execute(`
         INSERT INTO books (title, author) VALUES
