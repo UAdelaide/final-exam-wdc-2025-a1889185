91af7d432524b6165640b0e49fd44f279eee953d
James Walters <unknown@student.adelaide.edu.au>
2025-06-20 09:09:11 +0000

    [automatic commit] 2025-06-20 09:09:11 UTC 1.158.159.156

--- part1/app.js
+++ part1/app.js
@@ -100,14 +100,12 @@ let db;
     `);
 
     // Insert data
-    await db.execute(`
-        INSERT INTO Users (username, email, password_hash, role) VALUES
+    await db.execute(`INSERT INTO Users (username, email, password_hash, role) VALUES
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
         ('bobwalker', 'bob@example', 'hashed456', 'walker'),
         ('carol123', 'carol@example.com', 'hashed789', 'owner'),
         ('shaggy123', 'shaggy@mysteryinc.com', 'zoinkssc00b', 'owner'),
-        ('bruce123', 'brucewayne@wayneindustries.com', 'batman123', 'owner')
-      `);
+        ('bruce123', 'brucewayne@wayneindustries.com', 'batman123', 'owner')`);
     await db.execute(`
         INSERT INTO Dogs (owner_id, name, size) VALUES
         ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
