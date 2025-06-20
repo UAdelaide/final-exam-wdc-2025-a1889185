ac9c46c929e368e8f47ce253b45efb05d15c13c2
James Walters <unknown@student.adelaide.edu.au>
2025-06-20 04:21:36 +0000

    [automatic commit] 2025-06-20 04:21:36 UTC 1.158.159.156

--- part1/app.js
+++ part1/app.js
@@ -137,7 +137,7 @@ let db;
     if (rows[0].count === 0) {
       await db.execute(`
         INSERT INTO WalkApplications (request_id, walker_id, status) VALUES
-        
+        ((SELECT request_id FROM WalkRequests WHERE dog))
       `);
     }
     [rows] = await db.execute('SELECT COUNT(*) AS count FROM WalkRatings');
