65e533bfdc018997ed7231b91e4eb8b1a9ed5422
James Walters <unknown@student.adelaide.edu.au>
2025-06-20 03:55:08 +0000

    [automatic commit] 2025-06-20 03:55:08 UTC 1.158.159.156

--- part1/app.js
+++ part1/app.js
@@ -104,11 +104,11 @@ let db;
     if (rows[0].count === 0) {
       await db.execute(`
         INSERT INTO Users (username, email, password_hash, role) VALUES
-('alice123', 'alice@example.com', 'hashed123', 'owner'),
-('bobwalker', 'bob@example', 'hashed456', 'walker'),
-('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('shaggy123', 'shaggy@mysteryinc.com', 'zoinkssc00b', 'owner'),
-('bruce123', 'brucewayne@wayneindustries.com', 'batman123', 'owner');
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+        ('bobwalker', 'bob@example', 'hashed456', 'walker'),
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('shaggy123', 'shaggy@mysteryinc.com', 'zoinkssc00b', 'owner'),
+        ('bruce123', 'brucewayne@wayneindustries.com', 'batman123', 'owner')
       `);
     }
     [rows] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
