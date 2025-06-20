acbe936c1e89680589a5d9c948abb1821a67bafb
James Walters <unknown@student.adelaide.edu.au>
2025-06-20 05:09:26 +0000

    [automatic commit] 2025-06-20 05:09:25 UTC 1.158.159.156

--- part1/app.js
+++ part1/app.js
@@ -190,7 +190,10 @@ app.get('/api/walkrequests/open', async (req, res) => {
         WalkRequests.duration_minutes,
         WalkRequests.location,
         Users.username AS owner_username
-        
+      FROM WalkRequests
+      JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id
+      JOIN Users ON Dogs.owner_id = Users.user_id
+      WHERE WalkRequests.status = 'open'
         `);
     res.json(openRequests);
   } catch (err) {
