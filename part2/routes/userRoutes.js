8be5e250bd6841b244d6163f3c1fc4cb6ee765f9
James Walters <unknown@student.adelaide.edu.au>
2025-06-21 02:00:03 +0000

    [automatic commit] 2025-06-21 02:00:03 UTC 1.158.159.156

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -38,6 +38,7 @@ router.get('/me', (req, res) => {
 // POST login
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
+  console.log()
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
