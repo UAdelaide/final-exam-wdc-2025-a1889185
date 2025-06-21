de20e3259f70a86c99c09cfa91464e9f4bf0b81e
James Walters <unknown@student.adelaide.edu.au>
2025-06-21 03:10:56 +0000

    [automatic commit] 2025-06-21 03:10:56 UTC 1.158.159.156

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -56,7 +56,11 @@ router.post('/login', async (req, res) => {
 
 // POST logout
 router.post('/logout', (req, res) => {
-  req.session.destroy(err)
+  req.session.destroy(err => {
+    if (err) {
+      return res
+    }
+  })
 })
 
 module.exports = router;
