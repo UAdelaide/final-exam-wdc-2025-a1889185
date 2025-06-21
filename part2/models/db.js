f973d6f799ee0a9165dd5dc6713d4c516d57043e
James Walters <unknown@student.adelaide.edu.au>
2025-06-21 02:10:00 +0000

    [automatic commit] 2025-06-21 02:10:00 UTC 1.158.159.156

--- part2/models/db.js
+++ part2/models/db.js
@@ -4,9 +4,6 @@ const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   database: 'DogWalkService',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
 });
 
 module.exports = pool;
