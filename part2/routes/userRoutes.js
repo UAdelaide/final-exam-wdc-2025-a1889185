1339e263863ab61db56d18cc12bcbd1690804b44
James Walters <unknown@student.adelaide.edu.au>
2025-06-21 02:02:07 +0000

    [automatic commit] 2025-06-21 02:02:07 UTC 1.158.159.156

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -51,8 +51,7 @@ router.post('/login', async (req, res) => {
       console.log(rows[0].username, rows[0].role)
       req.session.user = {id: rows[0].user_id, username: rows[0].username, role: rows[0].role};
       res.json({ success: true, role: rows[0].role});
-    }
-    else {
+    } else {
       res.status(401).json({ success: false, message: 'Invalid credentials' });
     }
   catch (error) {
